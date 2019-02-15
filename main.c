63f08681fb792740797e92a41b8f10881c6c02a8
Park-SM <park97.sm@gmail.com>
2019-02-15 14:07:26 +0900

    refactoring about level

--- main.c
+++ main.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 #define LIFECOUNT 10
-#define LEVEL 3    // 3 ~ 5
 
 int ball = 0;
 int strike = 0;
 
-int CheckDuplicate(int *Answer){
+int CheckDuplicate(int *Answer, int LEVEL){
     int i, j;
     for (i = 0; i < LEVEL; i++)
         for(j = i + 1; j < LEVEL; j++)
               if(Answer[i] == Answer[j]) return 1;
-              
     return 0;
 }
 
-void ConstructGameAnswer(int *gAnswer){
+void ConstructGameAnswer(int *gAnswer, int LEVEL){
     int i;
     srand((unsigned)time(NULL));    // Construct randomize.
-    do{
-        for (i = 0; i < LEVEL; i++) gAnswer[i] = rand() % 10;    // Construct random.
-    }while(CheckDuplicate(gAnswer));  // To prevent duplication.
+    do{ for (i = 0; i < LEVEL; i++) gAnswer[i] = rand() % 10;    // Construct random.
+    }while(CheckDuplicate(gAnswer, LEVEL));  // To prevent duplication.
 }
 
-int CheckMatch(int *gAnswer, int *uAnswer){
+int CheckMatch(int *gAnswer, int *uAnswer, int LEVEL){
      int i, j;
-     for (i = 0; i < 4; i++){
-         for (j = 0; j < 4; j++){
+     for (i = 0; i < LEVEL; i++){
+         for (j = 0; j < LEVEL; j++){
              if( i == j ){
                  if (gAnswer[i] == uAnswer[j]) strike++;
              }else{
@@ -41,46 +37,48 @@ int CheckMatch(int *gAnswer, int *uAnswer){
      return 0;
 }
 
-int SetUsersAnswer(int *uAnswer,int life){
+void SetUsersAnswer(int *uAnswer,int life, int LEVEL){
+    int i;
+    char c[1];
     printf("\nLife %d) Input your Number: ", life);
-    switch(LEVEL){
-        case 3:
-             scanf("%1d%1d%1d", &uAnswer[0], &uAnswer[1], &uAnswer[2]);
-             break;
-        case 4:
-             scanf("%1d%1d%1d%1d", &uAnswer[0], &uAnswer[1], &uAnswer[2], &uAnswer[3]);
-             break;
-        case 5:
-             scanf("%1d%1d%1d%1d%1d", &uAnswer[0], &uAnswer[1], &uAnswer[2], &uAnswer[3], &uAnswer[4]);
-             break;
-        default:
-             system("cls");
-             puts("\nFatal Error:: Must be adjust LEVEL value to 3 ~ 5.\n");
-             return 1;
+    for (i = 0; i < LEVEL; i++){
+        c[0] = getchar();
+        if (c[0] == '\n' || c[0] == '\t' || c[0] == ' ') {i--; continue;}
+        uAnswer[i] = atoi(c);
     }
     while(getchar() != '\n');    // Empty the buffer.
-    return 0;
 }
 
 int main(){
     
     //// Setting ////
-    int life;
-    int gAnswer[LEVEL];
-    int uAnswer[LEVEL];
-    ConstructGameAnswer(gAnswer);
+    int l, life;
+    int Level;
+    int *gAnswer;
+    int *uAnswer;
+    do{
+       printf("> The number of digits in the game(0~10): ");
+       scanf("%d", &Level);       
+    }while(Level <= 0 || Level > 10);
+    printf("> Opportunities in the game: ");
+    scanf("%d", &life);
     
-    //for(life = 0; life < LEVEL; life++) printf("%d",gAnswer[life]); puts("");     // To confirm game's answer.
+    gAnswer = (int*)malloc(sizeof(int) * Level);
+    uAnswer = (int*)malloc(sizeof(int) * Level);
+    ConstructGameAnswer(gAnswer, Level);
+    system("cls");
+    
+    // for(life = 0; life < Level; life++) printf("%d",gAnswer[life]); puts("");     // To confirm game's answer.
     
     //// Running ////
-    puts("Game Start!!");
+    printf("Game Start!! (Level: %d)\n", Level);
     
-    for(life = LIFECOUNT; life > 0; life--){
-        if (SetUsersAnswer(uAnswer, life)) { system("PAUSE"); return 0; }   // Fatal Error.
-        if (CheckDuplicate(uAnswer)){
+    for(l = life; l > 0; l--){
+        SetUsersAnswer(uAnswer, l, Level);
+        if (CheckDuplicate(uAnswer, Level)){
             puts("> Please do not duplicate..");
         }else{
-            if(CheckMatch(gAnswer, uAnswer)){
+            if(CheckMatch(gAnswer, uAnswer, Level)){
                 printf("> %d Strike, %d Ball!\n", strike, ball);
                 puts("\n:::: You are Winner!!! ::::\n");
                 system("PAUSE");
@@ -94,8 +92,10 @@ int main(){
             puts("");
         }
     }
-    puts("\n:::: You are Loser ::::");
-  
+    printf("\n:::: You are Loser ::::\nGame answer: ");
+    for(life = 0; life < Level; life++) printf("%d",gAnswer[life]);
+    free(uAnswer);
+    free(gAnswer);
     system("PAUSE");   
     return 0;
 }
